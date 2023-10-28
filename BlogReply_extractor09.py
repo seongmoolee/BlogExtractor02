d27bac6d42cd7875a460b9e2c0067e70601923df
kate-05 <148893194+kate-05@users.noreply.github.com>
2023-10-28 20:28:26 +0900

    Add files via upload

---
+++ BlogReply_extractor09.py
@@ -0,0 +1,293 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import csv
+from os import path
+import os, random, re, sys
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
+import argparse
+import requests
+from tkinter import messagebox
+
+parser = argparse.ArgumentParser(description="블로그 추출기를 사용자 정의된 브라우저 위치 및 크기로 실행합니다.")
+parser.add_argument('--x-pos', type=int, help="브라우저 창의 X 위치")
+parser.add_argument('--y-pos', type=int, help="브라우저 창의 Y 위치")
+parser.add_argument('--width', type=int, help="브라우저 창의 너비")
+parser.add_argument('--height', type=int, help="브라우저 창의 높이")
+
+args = parser.parse_args()
+
+ # Wait and Scroll Logic
+def ScrollDowntoEnd(wait_class, direction):
+    print('스크롤 중...')
+    while True:
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        if direction == 'down':
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(sleeptime)
+        if direction == 'up': 
+            driver.execute_script("window.scrollBy(0, -30000);")
+            time.sleep(sleeptime)
+        try:
+            WebDriverWait(driver, 8 + sleeptime).until(EC.presence_of_element_located((By.CLASS_NAME, wait_class)))
+        except:
+            time.sleep(sleeptime)
+            pass
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+
+# LoadHTML(driver)
+def LoadHTML(driver):
+    html = driver.page_source
+    time.sleep(sleeptime)
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+# 사용자의 user_agent를 저장하는 파일 경로 설정
+USER_AGENT_FILE_PATH = './_SetFiles/라이선스.txt'
+
+def get_saved_user_agent():
+    """
+    저장된 user_agent를 반환합니다. 파일이 없으면 None을 반환합니다.
+    """
+    if os.path.exists(USER_AGENT_FILE_PATH):
+        with open(USER_AGENT_FILE_PATH, 'r') as file:
+            return file.read().strip()
+    else:
+        return None
+
+def save_user_agent(user_agent):
+    """
+    주어진 user_agent를 파일에 저장합니다.
+    """
+    with open(USER_AGENT_FILE_PATH, 'w') as file:
+        file.write(user_agent)
+        
+# Selenium 설정
+def Sel_set_driver():
+    chromedriver_autoinstaller.install()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    # 사용자 정의 창 위치 및 크기 인수가 제공된 경우 적용합니다.
+    if args.x_pos is not None and args.y_pos is not None:
+        options.add_argument(f"--window-position={args.x_pos},{args.y_pos}")
+    if args.width is not None and args.height is not None:
+        options.add_argument(f"--window-size={args.width},{args.height}")
+    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    # options.add_argument('user-agent=' + user_agent)
+    
+    # 사용자의 실제 user_agent를 가져옵니다.
+    user_agent = (
+        webdriver.Chrome(options=options)
+        .execute_script("return navigator.userAgent;")
+    )
+    
+    # if not os.path.exists(USER_AGENT_FILE_PATH):
+    #     os.makedirs(USER_AGENT_FILE_PATH, exist_ok=True)
+    #     save_user_agent(user_agent)
+        
+    
+    saved_user_agent = get_saved_user_agent()
+
+    # 이전에 user_agent가 저장되어 있고, 현재의 user_agent와 다르다면,
+    if saved_user_agent and saved_user_agent != user_agent:
+        messagebox.showerror(
+            "Error", "PC가 변경되었습니다. 새 라이선스가 필요합니다. 크롤링 크루에 문의하세요."
+        )
+        sys.exit("프로그램을 종료합니다.")  # 프로그램을 종료합니다.
+
+    # 처음 실행이거나 user_agent가 같다면, 현재 user_agent를 저장합니다.
+    if not saved_user_agent:
+        save_user_agent(user_agent)
+        messagebox.showerror(
+            "라이선스 등록 알림",
+            "사용하고 계신 PC가 등록되었습니다. 다른 PC에서는 사용이 제한됩니다."
+        )
+    time.sleep(2)
+    options.add_argument(f'user-agent={user_agent}')
+    
+    
+    
+    #크롬창 정보
+    # if args.x_pos is not None and args.y_pos is not None:
+    #     options.add_argument(f"--window-position={args.x_pos},{args.y_pos}")
+    # else: pass
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+# 인터넷속도 = time sleep 속도 읽어오기
+timesleep_file = f'./_SetFiles/인터넷속도.txt'
+if os.path.exists(timesleep_file):
+    with open(timesleep_file, 'r') as f:
+        sleeptime = int(f.read().strip())
+else: 
+    with open(timesleep_file, 'w') as f:
+        f.write(2)
+        
+# Load base URLs and blog names from CSV
+with open('./_SetFiles/blog_list.csv', 'r', encoding='cp949') as file:
+    reader = csv.reader(file)
+    next(reader)  # 헤더 건너뛰기
+    blog_entries = []
+    for row in reader:
+        blog_entries.append((row[0], row[1]))
+
+for blog_name, blog_url in blog_entries:
+    # print(blog_url)
+    blog_ID = blog_url.split('blog.naver.com/')[1].split('/')[0]
+    m_blog_url = f"https://m.blog.naver.com/{blog_ID}"
+    pc_blog_url = f"https://blog.naver.com/{blog_ID}"
+    base_url = f"https://m.blog.naver.com/PostList.naver?blogId={blog_ID}&categoryNo=0&from=postList&listStyle=post"
+    driver = Sel_set_driver()
+    #Target Directory 생성
+    TargetDir01 = f'./결과파일/{blog_name}/MidFiles'
+    if os.path.exists(TargetDir01)==False:
+        os.makedirs(TargetDir01, exist_ok=True)
+    else: pass
+        
+    # 전체 포스트 링크 및 댓글/공감 유무 추출
+    PostList_file = f'{TargetDir01}/{blog_name}_PostList.txt'
+    IsFile = os.path.exists(PostList_file)
+    main_links_info = []  # This will store tuples of (link, has_like, has_comment)
+    if IsFile == False:
+        sleeptime = 3
+        driver.switch_to.window(driver.window_handles[0])
+        driver.get(base_url)
+        time.sleep(sleeptime)
+        ScrollDowntoEnd('link__OVpnJ', 'down')
+        soup = LoadHTML(driver)
+        
+        postlist_div = soup.find('div', class_='list__fJdGM')
+        
+        for post in postlist_div.find_all('div', class_='postlist__LXY3R'):
+            link = post.find('a', class_='link__OVpnJ')
+            if link:
+                has_like = bool(post.find('span', class_='like____2o5'))
+                has_comment = bool(post.find('span', class_='comment___IpyZ'))
+                main_links_info.append((link['href'], has_like, has_comment))
+        
+        print('추출된 글개수: ',len(main_links_info))
+
+        # Saving the main_links_info to PostList_file
+        with open(PostList_file, 'w', newline='\n') as f:
+            for link_info in main_links_info:
+                f.write(','.join(map(str, link_info)) + '\n')
+                
+    else:
+        with open(PostList_file, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                link, has_like, has_comment = line.strip().split(',')
+                main_links_info.append((link, has_like == 'True', has_comment == 'True'))
+        print('추출된 글개수: ',len(main_links_info))
+
+                
+            
+    # 인터넷속도 = time sleep 속도 다시 읽어오기
+    with open(timesleep_file, 'r') as f:
+        sleeptime = int(f.read().strip())        
+      
+
+    last_i_value_filename = f'{TargetDir01}/{blog_name}_last_ivalue.txt'
+    if os.path.exists(last_i_value_filename):
+        with open(last_i_value_filename, 'r') as f:
+            start_i = int(f.read().strip())
+    else:
+        start_i = 0
+    
+    전체포스트갯수 = len(main_links_info)
+    all_ids = set()
+    i = start_i
+    FLAG1 = False
+    for link, has_like, has_comment in main_links_info[start_i:]:
+        link = link
+        has_like = has_like == True
+        has_comment = has_comment == True
+        
+        try:
+            # print('link', link)
+            post_id_mid = link.replace(f'https://m.blog.naver.com/{blog_ID}/','')
+            post_id = post_id_mid.replace('?referrerCode=1','')
+            # print('\npost_id', post_id)
+            
+
+            # Extract IDs from like_more_link 공감
+            if has_like:
+                print(f"\n공감 DB 추출 중.. 전체 {전체포스트갯수}개 글 중 {i+1}번째 글")
+                current_ids_like = set()
+                like_more_link = f'https://m.blog.naver.com/SympathyHistoryList.naver?blogId={blog_ID}&logNo={post_id}&categoryId=POST'
+                # print('like_more_link_sleeptime:', sleeptime)
+                driver.get(like_more_link)
+                time.sleep(1)
+                try:
+                    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'num___9TRf')))
+                except:pass
+                total_num = int(driver.find_element(By.CLASS_NAME, 'num___9TRf').text.replace(',',''))
+                # print(f"공감 DB의 총 갯수는 {total_num}개 입니다.")
+                ScrollDowntoEnd('link__D9GoZ', 'down')
+                soup = LoadHTML(driver)
+                try:
+                    # bloger_area___eCA_ 아래의 link__D9GoZ 클래스를 가진 모든 a tag를 수집합니다.
+                    for a_tag in soup.select('div.bloger_area___eCA_ a.link__D9GoZ'):
+                        extracted_id = a_tag['href'].replace('https://m.blog.naver.com/', '')
+                        current_ids_like.add(extracted_id)
+                    all_ids.update(current_ids_like)
+                except: pass
+                print(f"{i+1}번째 글의 공감에서 {len(current_ids_like)}개의 ID를 추출했습니다.")
+            else:
+                print(f"\n전체 {전체포스트갯수}개 글 중 {i+1}번째 글은 공감을 허용하지 않는 포스트입니다.")
+                
+            # Extract IDs from reply_link 댓글
+            if has_comment:
+                print(f"\n댓글 DB 추출 중.. 전체 {전체포스트갯수}개 글 중 {i+1}번째 글")
+                current_ids_reply = set()
+                reply_link = f'https://m.blog.naver.com/CommentList.naver?blogId={blog_ID}&logNo={post_id}'
+                # print('reply_link', reply_link)
+                driver.get(reply_link)
+                time.sleep(1)
+                try:
+                    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'num___9TRf')))
+                except: pass
+                total_num = int(driver.find_element(By.CLASS_NAME, 'num___9TRf').text.replace(',',''))
+                # print(f"댓글의 총 갯수는 {total_num}개 입니다.")
+                ScrollDowntoEnd('u_cbox_name', 'up')
+                soup = LoadHTML(driver)
+                try:
+                    for a_tag in soup.find_all('a', class_='u_cbox_name'):
+                        current_ids = a_tag['href'].replace('https://m.blog.naver.com/PostList.naver?blogId=', '')
+                        current_ids_reply.add(current_ids)
+                    all_ids.update(current_ids_reply)
+                except: pass
+                print(f"{i+1}번째 글의 댓글에서 {len(current_ids_reply)}개의 ID를 추출했습니다.")
+            else:
+                print(f"\n전체 {전체포스트갯수}개 글 중 {i+1}번째 글은 댓글을 허용하지 않는 포스트입니다.")
+            
+            output_filename = f"./결과파일/{blog_name}/{blog_name}_output.csv"
+            with open(output_filename, 'a', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                # writer.writerow(["Email"])
+                for id in all_ids:
+                    writer.writerow([id + "@naver.com"])
+            all_ids.clear()
+            i = i + 1
+            with open(last_i_value_filename, 'w') as f:
+                f.write(str(i))
+                
+        except:
+            FLAG1 = True 
+            # driver.quit()
+            break
+        
+    if FLAG1 == True:
+        # FLAG2 = True
+        break
+
+# Close the browser
+driver.quit()
---
+++ initiator_05.py
@@ -0,0 +1,293 @@
+import os, csv
+import subprocess
+import time
+import shutil
+import psutil
+import pandas as pd
+import configparser
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+from selenium import webdriver
+import pandas as pd
+import traceback
+import tkinter as tk
+import threading
+import ctypes
+from tkinter import Entry, Label, Button, Text, Scrollbar, END
+from queue import Queue, Empty  # Empty를 직접 import 해야 합니다.
+import queue
+
+
+
+
+
+def is_admin():
+    try:
+        # 관리자 권한으로 실행되고 있는지 확인합니다.
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+def copy_folder():
+    source = "C:\\Program Files (x86)\\Google"
+    destination = "C:\\Program Files\\Google"
+
+    if not os.path.exists(source):
+        # 소스 디렉토리가 존재하지 않으면 아무 것도 하지 않고 종료
+        return
+
+    if os.path.exists(destination):
+        
+        # 목적지 디렉토리가 이미 존재하면 아무 것도 하지 않고 종료
+        return
+
+    # 디렉토리 복사 (하위 디렉토리 및 파일 포함)
+    if is_admin():
+        shutil.copytree(source, destination)
+
+blog_name = ""
+blog_url = ""
+browser_position =""
+exe_file="./dist/BlogReply_extractor09.exe"
+
+
+# GUI 창 생성 및 구성
+root = tk.Tk()
+root.title("블로그 정보 입력")
+
+tk.Label(root, text="블로그 이름").grid(row=0, padx=20, pady=5)
+tk.Label(root, text="블로그 URL").grid(row=1, padx=20, pady=5)
+
+blog_name_entry = tk.Entry(root, width = 30)
+blog_name_entry.grid(row=0, column=1, padx=20, pady=5)
+
+blog_url_entry = tk.Entry(root, width = 30)
+blog_url_entry.grid(row=1, column=1, padx=20, pady=5)
+
+# 로그 출력을 위한 Text 위젯과 Scrollbar 추가
+log_text = Text(root, wrap="word", width=50, height=10)
+log_text.grid(row=4, column=0, columnspan=2, sticky="nsew")
+scrollbar = Scrollbar(root, command=log_text.yview)
+scrollbar.grid(row=4, column=2, sticky="ns")
+log_text.config(yscrollcommand=scrollbar.set)
+
+
+
+
+# 로그 메시지를 GUI에 출력하는 함수
+def log(message):
+    def _log():
+        log_text.insert(END, message + "\n")
+        log_text.yview(END)
+    
+    root.after(0, _log)  # 메인 스레드에서 _log 함수 실행
+    
+    
+main_links_info=[]
+def run_exe_if_required():
+    blog_name = blog_name_entry.get()
+    blog_url = blog_url_entry.get()
+    blog_entries = [(blog_name, blog_url)]
+    for blog_name, blog_url in blog_entries:
+        
+        try:
+            if not os.path.exists(results_base_dir) or not os.path.exists(mid_files_dir):
+                # subprocess.call([exe_file])
+                threaded_start_extractor()
+                # while_remain_post()
+                
+            elif os.path.exists(last_i_value_filename) and os.path.exists(PostList_file):
+                if os.path.exists(exe_file):
+                    while_remain_post()
+            
+            else:
+                shutil.rmtree(results_base_dir)
+                log(f"{blog_name} 폴더를 삭제했습니다. 프로그램을 재실행해 주세요")
+                input("Press Enter to continue...")
+                
+        except Exception as e:
+            log(f"def run_exe_if_required: {e}")
+            log(traceback.format_exc()) 
+            
+            
+def is_exe_running(exe_file):
+    for process in psutil.process_iter():
+        try:
+            if exe_file.lower() == process.exe().lower():
+                return True
+        except Exception as e:
+            log(f"is_exe_running: {e}")
+            log(traceback.format_exc())
+            log("\n봇방지로 인해 프로그램이 종료되었습니다..")
+    return False
+
+def restart_exe(exe_file):
+    if not is_exe_running(exe_file):
+        log('DB 추출 실행 파일을 재시작합니다.')
+        threaded_start_extractor()
+
+def remove_duplicate():
+    # 중복 데이터 제거
+    data = pd.read_csv(output_csv)
+    data_no_duplicates = data.drop_duplicates()
+
+    # 최종DB.csv로 저장
+    data_no_duplicates.to_csv(final_csv, index=False)
+
+    # output.csv 파일을 다른 폴더로 이동
+    if not os.path.exists(mid_files_dir):
+        os.mkdir(mid_files_dir)
+    shutil.move(output_csv, f'{mid_files_dir}/{blog_name}_output.csv')
+    log(f"{final_csv} 파일에 추출이 완료되었습니다.")
+    FLAG01 = True
+    input("Press Enter to continue...")
+    
+def while_remain_post():
+    with open(PostList_file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            link, has_like, has_comment = line.strip().split(',')
+            main_links_info.append((link, has_like == 'True', has_comment == 'True'))
+        
+    with open(last_i_value_filename, 'r') as f:
+        last_i = int(f.read().strip())
+    
+    while last_i < len(main_links_info):
+        link, has_like, has_comment = main_links_info[last_i]
+        has_like = has_like == True
+        has_comment = has_comment == True
+                
+        # 그외에는 이전과 동일한 프로세스를 수행합니다.
+        with open(last_i_value_filename, 'r') as f:
+            last_i = int(f.read().strip())
+        threaded_start_extractor()
+
+        
+        while True:
+            time.sleep(3)
+            restart_exe(exe_file)
+            break
+    else:
+        remove_duplicate()
+
+FLAG_Submit = False  
+def submit_action():
+    global blog_name
+    global blog_url
+    blog_entries = [(blog_name, blog_url)]
+    blog_name = blog_name_entry.get()
+    blog_url = blog_url_entry.get()
+    
+
+    # GUI의 현재 위치와 크기를 가져옵니다.
+    gui_x = root.winfo_rootx()
+    gui_y = root.winfo_rooty()
+    gui_width = root.winfo_width()
+    # gui_height = root.winfo_height()
+    
+    # 창의 위치 정보를 전역 변수에 저장합니다.
+    global browser_position
+    browser_position = (gui_x + gui_width, gui_y)  # 창 사이의 작은 간격을 위해 10을 추가합니다.
+    save_and_initiate()
+    
+    # start_extractor를 스레드에서 실행합니다.
+    # thread = threading.Thread(target=start_extractor,args=(exe_file, log_text, browser_position))
+    # thread.start()
+
+def save_and_initiate():
+    filename = './_SetFiles/blog_list.csv'
+    with open(filename, 'w', newline='', encoding='cp949') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['blog_name', 'blog_url'])
+        writer.writerow([blog_name, blog_url])
+
+    run_exe_if_required()
+    
+    
+def check_log_queue():
+    try:
+        while True:
+            text = log_queue.get_nowait()  # 큐에서 로그 메시지를 가져옵니다.
+            # 실제 로그 출력 처리 (예: 텍스트 위젯에 메시지 출력)
+            log_text.insert("end", text)  # 'log_text'는 로그 메시지를 표시하는 Tkinter 위젯일 수 있습니다.
+    except queue.Empty:
+        # 큐가 비어 있으면 아무 것도 하지 않습니다.
+        pass
+
+    # 큐를 계속 확인합니다.
+    root.after(100, check_log_queue)  # 'root'는 Tkinter의 메인 창 인스턴스입니다.
+
+
+# 로그 메시지를 저장할 큐 생성
+log_queue = queue.Queue()
+def start_extractor(exe_file, log_text, browser_position):
+    # global browser_position
+    x, y = browser_position
+
+    def read_stream(stream, callback):
+        while True:
+            line = stream.readline()  # 스트림에서 한 줄씩 읽기
+            if line:  # line이 빈 문자열이면 EOF(파일의 끝)을 의미합니다.
+                callback(line)
+            else:
+                break  # EOF에 도달하면 반복을 종료합니다.
+
+    def stream_output(process):
+        def enqueue_output(line):
+            log_queue.put(line)  # 큐에 로그 메시지를 추가합니다.
+        # 표준 출력과 표준 에러를 각각 다른 스레드로부터 읽어옵니다.
+        stdout_thread = threading.Thread(target=read_stream, args=(process.stdout, enqueue_output))
+        stderr_thread = threading.Thread(target=read_stream, args=(process.stderr, enqueue_output))
+
+        stdout_thread.start()
+        stderr_thread.start()
+
+        stdout_thread.join()  # 스레드가 종료될 때까지 기다립니다.
+        stderr_thread.join()
+
+        process.stdout.close()  # 스트림을 닫습니다.
+        process.stderr.close()
+        process.wait()  # 프로세스가 종료될 때까지 기다립니다.
+
+    # subprocess.Popen으로 외부 프로세스를 시작합니다.
+    process = subprocess.Popen(
+        [exe_file, "--x-pos", str(x), "--y-pos", str(y)],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        text=True,  # text 모드로 설정, universal_newlines=True와 같습니다.
+        bufsize=1,  # 줄 단위로 버퍼링
+    )
+    check_log_queue()
+    # 출력을 처리할 스레드를 생성하고 시작합니다.
+    output_thread = threading.Thread(target=stream_output, args=(process,))
+    output_thread.start()
+
+def threaded_start_extractor():
+    # 'exe_file', 'log_text', 'browser_position'을 인자로 추가해야 합니다.
+    extractor_thread = threading.Thread(target=start_extractor, args=(exe_file, log_text, browser_position))
+    extractor_thread.start()
+# 스레드를 시작합니다.
+
+
+
+submit_button = tk.Button(root, text="제출", command=submit_action)
+submit_button.grid(row=2, column=1, pady=20)
+
+gui_width = root.winfo_width()
+gui_x = root.winfo_x()
+gui_y = root.winfo_y()
+
+# 파일 및 디렉터리 경로 생성
+base_dir = '.'  # 기본 디렉터리 설정
+results_base_dir = os.path.join(base_dir, '결과파일', blog_name_entry.get())
+mid_files_dir = os.path.join(results_base_dir, 'MidFiles')
+
+# 파일 경로 설정
+timesleep_file = os.path.join(base_dir, '_SetFiles', '인터넷속도.txt')
+output_csv = os.path.join(results_base_dir, f'{blog_name_entry.get()}_output.csv')
+final_csv = os.path.join(results_base_dir, f'{blog_name_entry.get()}_최종DB.csv')
+last_i_value_filename = os.path.join(mid_files_dir, f'{blog_name_entry.get()}_last_value.txt')
+PostList_file = os.path.join(mid_files_dir, f'{blog_name_entry.get()}_PostList.txt')
+
+root.mainloop()
+
+
